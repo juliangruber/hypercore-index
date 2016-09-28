266ee4bad1adee26a4ca113d680805259af6d38f
Julian Gruber <julian@juliangruber.com>
2016-09-28 18:09:50 +0200

    add simple test. #3

---
+++ test.js
@@ -0,0 +1,34 @@
+'use strict'
+
+var hypercore = require('hypercore')
+var level = require('memdb')
+var fs = require('fs')
+var index = require('./')
+var test = require('tap').test
+
+test('index', function (t) {
+  t.plan(3)
+
+  var core = hypercore(level())
+  var feed = core.createFeed()
+
+  fs.createReadStream(__filename)
+  .pipe(feed.createWriteStream())
+  .on('finish', function () {
+    index(feed, {
+      live: false
+    }, function (entry, cb) {
+      t.ok(entry)
+      cb()
+    }, function (err) {
+      t.error(err)
+      index(feed, {
+        live: false
+      }, function (entry, cb) {
+        t.fail('Did not resume')
+      }, function (err) {
+        t.error(err)
+      })
+    })
+  })
+})
